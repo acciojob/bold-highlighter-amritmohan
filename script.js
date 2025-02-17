6c9eeb76816752ebb507a7726dd66d33faf559d6
@amritmohan <amritmohan2284@gmail.com>
2025-02-17 10:27:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
-    //Write your code here
-
-
+  const boldWords = document.querySelectorAll('strong');
+  
+  // Loop through each <strong> element and change its color to green
+  boldWords.forEach(word => {
+    word.style.color = 'rgb(0, 128, 0)';
+  });
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+  const boldWords = document.querySelectorAll('strong');
+  
+  // Loop through each <strong> element and revert its color to black
+  boldWords.forEach(word => {
+    word.style.color = 'rgb(0, 0, 0)';
+  });
 }
